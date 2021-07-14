ac47ed341f14f5037b7a64cb6cee0ed3554fbdfa
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2021-07-14 10:27:06 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -2,10 +2,16 @@ library("rjson")
 library(tidyverse)
 library(shiny)
 
+choices = list()
+json_file <- fromJSON(file = "test4.json")
+for (i in 1:length(json_file["data"][[1]])){
+    choices[[paste(json_file["data"][[1]][[i]][["Study Title"]]," (",json_file["data"][[1]][[i]][["genestack:accession"]],")", sep = "")]] <- i
+}
+
 # Give the input file name to the function.
 options(DT.options = list(pageLength = 50))
-format_json <- function(file_name) {
-    json_file <- fromJSON(file = file_name)
+format_json <- function(study_number) {
+    json_file <- (json_file["data"][[1]][[as.integer(study_number)]])
     json_data_frame = map(json_file, ~ str_c(., collapse = "<br>")) %>% as_tibble
     transposed = as_tibble(cbind(nms = names(json_data_frame), t(json_data_frame)))
     return(transposed)
@@ -14,7 +20,7 @@ format_json <- function(file_name) {
 
 ui <- fluidPage(
     selectInput("select", label = h3("Projects"), 
-                choices = list("Project 1" = "test1.json", "Project 2" = "test2.json", "Project 3" = "test3.json"), 
+                choices,  
                 selected = 1),
     
     DT::dataTableOutput("mytable") 
